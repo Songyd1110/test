12864a38b169ce40a7c3d16c5637f70f53511531
songyd <841549912@qq.com>
2018-11-21 14:43:29 +0800

    commit branch test13

--- src/test/TestJDK.java
+++ src/test/TestJDK.java
@@ -11,39 +11,43 @@ import java.util.stream.Stream;
 
 public class TestJDK {
 	public static void main(String[] args) throws IOException {
-		/*List<String> l = Stream.of("one", "two", "three", "four")
-		 .filter(e -> e.length() > 3)
-		 .peek(e -> System.out.println("Filtered value: " + e))
-		 .map(String::toUpperCase)
-		 .peek(e -> System.out.println("Mapped value: " + e))
-		 .collect(Collectors.toList());
-		System.out.println(l);*/
-		/*Stream.iterate(0, n -> n + 3).limit(10). forEach(x -> System.out.print(x + " "));
-		Stream.iterate(2, n->n*3).limit(10).forEach(n->System.out.println(n+""));*/
+		/*
+		 * List<String> l = Stream.of("one", "two", "three", "four") .filter(e
+		 * -> e.length() > 3) .peek(e -> System.out.println("Filtered value: " +
+		 * e)) .map(String::toUpperCase) .peek(e -> System.out.println(
+		 * "Mapped value: " + e)) .collect(Collectors.toList());
+		 * System.out.println(l);
+		 */
+		/*
+		 * Stream.iterate(0, n -> n + 3).limit(10). forEach(x ->
+		 * System.out.print(x + " ")); Stream.iterate(2,
+		 * n->n*3).limit(10).forEach(n->System.out.println(n+""));
+		 */
 		BufferedReader br = new BufferedReader(new FileReader("E:\\11.txt"));
 		String st = br.readLine();
 		System.out.println(st);
-		System.out.println(st);
+		System.out.println(st+"asdadadada");
 		System.out.println(st);
 		System.out.println(st);
 
-		//test commit master
+		// test commit master
 
-		//test commit branch
+		// test commit branch
 
 		System.out.println(st);
-		
-	/*	List<String> words = br.lines().
-				 flatMap(line -> Stream.of(line.split(" "))).
-				 filter(word -> word.length() > 0).
-				 map(String::toLowerCase).
-				 distinct().
-				 sorted().
-				 collect(Collectors.toList());
-				br.close();
-				System.out.println(words);*/
-		/*List<String> words  = br.lines().flatMap(line -> Stream.of(line.split(" "))).map(String::toUpperCase).distinct().sorted().collect(Collectors.toCollection( ArrayList ::new ));
-		System.out.println(words);*/
+
+		/*
+		 * List<String> words = br.lines(). flatMap(line ->
+		 * Stream.of(line.split(" "))). filter(word -> word.length() > 0).
+		 * map(String::toLowerCase). distinct(). sorted().
+		 * collect(Collectors.toList()); br.close(); System.out.println(words);
+		 */
+		/*
+		 * List<String> words = br.lines().flatMap(line -> Stream.of(line.split(
+		 * " "
+		 * ))).map(String::toUpperCase).distinct().sorted().collect(Collectors.
+		 * toCollection( ArrayList ::new )); System.out.println(words);
+		 */
 	}
 
 }
